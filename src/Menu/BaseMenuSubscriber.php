b6398b46d969f90748dc3622a7268a0aa46f4c05
Tac Tacelosky <tacman@gmail.com>
2022-02-17 06:38:43 -0600

    check for null route

--- src/Menu/BaseMenuSubscriber.php
+++ src/Menu/BaseMenuSubscriber.php
@@ -174,8 +174,11 @@ class BaseMenuSubscriber
                          'show' => 'fas fa-eye',
                          'edit' => 'fas fa-wrench'
                      ] as $regex=>$icon) {
-                if (preg_match("|$regex|", $options['route'])) {
-                    $options['data-icon'] = $icon;
+
+                if ($route = $options['route']) {
+                    if (preg_match("|$regex|", $route)) {
+                        $options['data-icon'] = $icon;
+                    }
                 }
             }
         }
